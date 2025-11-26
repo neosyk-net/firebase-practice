591a2a5d3132c523604098e039916df6b6c22662
neosyk-net <runerdycastillo@gmail.com>
2025-11-26 16:59:28 -0500

    Intro to CRUD

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import { auth } from "./firebase/init";
+import { auth, db } from "./firebase/init";
+import { collection, addDoc } from "firebase/firestore";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -11,6 +12,15 @@ import {
 function App() {
   const [user, setUser] = React.useState({});
   const [loading, setLoading] = React.useState(true);
+
+  function createPost() {
+    const post = {
+      title: "Land a 200k job",
+      description: "Finish FES!"
+    }
+    addDoc(collection(db, "posts"), post)
+  }
+
 
   React.useEffect(() => {  
     onAuthStateChanged(auth, (user) => {
@@ -53,6 +63,7 @@ function App() {
       <button onClick={login}>Login</button>
       <button onClick={logout}>Logout</button>
       {loading ? 'loading...' : user.email}
+      <button onClick={createPost}>Create Post</button>
     </div>
   );
 }
