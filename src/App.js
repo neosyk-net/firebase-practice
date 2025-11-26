541cda1dd55aca00b9ef58a9f2c25dc582bf8d18
neosyk-net <runerdycastillo@gmail.com>
2025-11-25 22:56:40 -0500

    firebase into complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import React from "react";
 import "./App.css";
 import { auth } from "./firebase/init";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
 } from "firebase/auth";
 
 function App() {
+  const [user, setUser] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {  
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      console.log(user);
+      if (user) {
+        setUser(user);
+      }
+    })
+    }, []);
+
   function register() {
-    createUserWithEmailAndPassword(auth, "email@email.com", "test123")
+    createUserWithEmailAndPassword(auth, "email2@email.com", "test123")
       .then((user) => {
         console.log(user);
       })
@@ -17,19 +33,26 @@ function App() {
   }
 
   function login() {
-    signInWithEmailAndPassword(auth, "email@email.com", "test123")
-      .then((user) => {
-        console.log(user);
+    signInWithEmailAndPassword(auth, "email2@email.com", "test123")
+      .then(({ user }) => {
+        setUser(user);
       })
       .catch((error) => {
         console.log(error);
       });
   }
 
+  function logout() {
+    signOut(auth);
+    setUser({});
+  }
+
   return (
     <div className="App">
       <button onClick={register}>Register</button>
       <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+      {loading ? 'loading...' : user.email}
     </div>
   );
 }
