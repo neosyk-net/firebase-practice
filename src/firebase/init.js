df5c10aee0e312233a198f6b24019eee424c3f36
neosyk-net <runerdycastillo@gmail.com>
2025-11-25 20:11:58 -0500

    Login Button

---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzS7G0WN2TRRIKIGvBYqqNztcGeyMYMMQ",
+  authDomain: "fir-practice-ad59e.firebaseapp.com",
+  projectId: "fir-practice-ad59e",
+  storageBucket: "fir-practice-ad59e.firebasestorage.app",
+  messagingSenderId: "494533191694",
+  appId: "1:494533191694:web:b34686417e61d84d21345f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
